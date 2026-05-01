c9caae6c327bd48dcbac2705b29cbba9a637a196
LeFab70 <kouonang2002@gmail.com>
2026-05-01 10:48:32 -0300

    final

--- NewsAppMVVM_Fab/NewsApp/Constants.Local.cs
+++ NewsAppMVVM_Fab/NewsApp/Constants.Local.cs
@@ -3,6 +3,6 @@ namespace NewsApp;
 // Fichier local (NON versionné) : place ta clé ici.
 public static class ConstantsLocal
 {
-    public const string NewsApiKey = "1e9ea41f330f4113b4c5c142160eb57f";
+    public const string NewsApiKey = "a7e95874a45e42ad885010e6ff6096dc";//1e9ea41f330f4113b4c5c142160eb57f;
 }
 
